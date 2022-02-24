7cfa11046523210e8c721407e9518717ef583b89
IT21245756 D.A.P.M.M.Perera <98573138+PivithuruMilan@users.noreply.github.com>
2022-02-24 05:24:42 +0000

    tute02.cpp tute03.cpp tute04.cpp

--- tute04.cpp
+++ tute04.cpp
@@ -12,18 +12,43 @@ long nCr(int n, int r);
 Do not modify the main function.*/
 
 #include <iostream>
+using namespace std;
 
 long Factorial(int no);
-long nCr(int n, int r);
+long nCr(int nfac, int rfac,int ifac);
 
 int main() {
-  int n, r;
-  std::cout << "Enter a value for n ";
-  std::cin >> n;
-  std::cout << "Enter a value for r ";
-  std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
-  std::cout << std::endl;
+  int n, r,i,nfac,rfac,ifac;
+  cout << "Enter a value for n ";
+  cin >> n;
+  nfac=Factorial(n);
+  cout << "Enter a value for r ";
+  cin >> r;
+  rfac = Factorial(r);
+  i = n - r;
+  ifac = Factorial(i);
+  cout << "nCr = ";
+  cout << nCr(nfac,rfac,ifac);
+  cout << endl;
   return 0;
 }
+long Factorial(int no)
+{
+  int x,fact = 1;
+    for(x = no ; x >= 1 ;x--)
+      {
+         fact = x * fact;
+    
+      }
+return fact;    
+}
+  
+long nCr(int nfac,int rfac,int ifac)
+{
+long answer;
+  answer = nfac /(rfac * ifac );
+  return answer;
+}
+
+
+  
